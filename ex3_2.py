820357d52e646312b7bef708b4e6bcf4f8aeb593
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-04-01 14:00:01 -0700

    Add files via upload

---
+++ ex3_2.py
@@ -0,0 +1,9 @@
+# Receive inputs from user
+name = input('Please type your name: ')
+print('Your name is ' + name)
+
+# Quiz
+your_name = input("Enter your name: ")
+fruit = input("Enter your favorite fruit: ")
+
+print(your_name + ' likes ' + fruit + '.')
