2285670125f906679b001b202b7bc286e50e5492
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-04-06 22:44:48 -0700

    Add files via upload

---
+++ ex4_1.py
@@ -0,0 +1,12 @@
+# Calculate User Age
+birth_year = input('Enter your birth year: ')
+age = 2023 - int(birth_year)
+print(age)
+
+# Data Type Conversion
+
+print(int(3.14))
+print(float(10))
+print(str(100))
+print(bool(-10), bool('hello'), bool(3.14))
+print(bool(0), bool(''), bool(None))
