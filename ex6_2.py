cd44384e792ba478adb4d65402497e2a8391b4b5
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-04-08 14:48:00 -0700

    Add files via upload

--- ex6_2.py
+++ ex6_2.py
@@ -8,6 +8,6 @@ message = '{} likes {}.'.format(your_name, fruit)
 message = '{1} likes {0}.'.format(fruit, your_name)
 print(message)
 
-# f-string
+# F-String
 message = f'{your_name} likes {fruit}.'
 print(message)
