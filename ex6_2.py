8f0a3e4334c681a28477d4eac79c230adcd858d0
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-04-08 14:47:08 -0700

    Add files via upload

---
+++ ex6_2.py
@@ -0,0 +1,13 @@
+# Strings
+your_name = 'HongTu'
+fruit = 'Apples'
+quantity = 10.0
+
+# String Format
+message = '{} likes {}.'.format(your_name, fruit)
+message = '{1} likes {0}.'.format(fruit, your_name)
+print(message)
+
+# f-string
+message = f'{your_name} likes {fruit}.'
+print(message)
