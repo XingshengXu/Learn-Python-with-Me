45960738371eb8bff4e596054180ce1050e890e4
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-04-08 14:31:08 -0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+# Common Math Functions
+x = round(3.14)
+x = abs(-10)
+
+print(x)
+
+# Math Module
+import math
+
+
+a = math.pi
+a = math.floor(3.14)
+a = math.ceil(3.14)
+print(a)
+
+print(dir(math))
+print(help(math))
+print(help(math.floor))
