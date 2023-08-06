2ef1269c2efe49c02a1f60aa12a46c7f40fe522d
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-08-05 17:33:40 -0700

    Add files via upload

---
+++ Python Intermediate Syntax Lesson Codes/ex26/ex26.py
@@ -0,0 +1,23 @@
+time = int(input('working time: '))
+print(time)
+
+
+# 异常处理 Exception Handling
+# try:
+#     time = int(input('working time: '))
+#     if time < 20:
+#         raise Exception('Time cannot be less than 20')
+#     income = 20000
+#     average_income = income / time
+#     # print(time)
+#     print(average_income)
+# except ValueError:
+#     print('Invalid age value')
+# except ZeroDivisionError:
+#     print('Time cannot be 0')
+# except Exception as e:
+#     print(e)
+# else:
+#     print('Calculation complete')
+# finally:
+#     print('Program closed')
