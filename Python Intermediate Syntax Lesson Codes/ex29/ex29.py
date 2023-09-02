f0b1d38796d836f1683eec93b8d1006f4558b8fe
CyberHongTu <125934684+XingshengXu@users.noreply.github.com>
2023-09-02 12:47:30 -0700

    Add files via upload

--- Python Intermediate Syntax Lesson Codes/ex29/ex29.py
+++ Python Intermediate Syntax Lesson Codes/ex29/ex29.py
@@ -1,34 +1,34 @@
 # 类的变量 (Class Variables)
 class Mammal:
     class_name = 'Mammalia'
-    warm_blooded = True
-
-    # @classmethod
-    # def print_class_name(cls):
-    #     return f"Class name is called: {cls.class_name}"
-
-    # @classmethod
-    # def from_birth_year(cls, name, birth_year):
-    #     age = 2023 - birth_year
-    #     return cls(name, age)
-
-    # @staticmethod
-    # def introduction():
-    #     print("Mammals are a class of warm-blooded vertebrates that "
-    #           "have hair or fur, and three middle ear bones.")
+    warm_blood = True
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    @classmethod
+    def from_birth_year(cls, name, birth_year):
+        age = 2023 - birth_year
+        return cls(name, age)
+
+    @classmethod
+    def print_class_name(cls):
+        return f"Class name is called: {cls.class_name}."
+
+    @staticmethod
+    def introduction():
+        print("Mammals are a class of warm-blooded vertebrates that "
+              "have hair or fur, and three middle ear bones.")
+
     def feed(self):
         print("Feeding")
 
-    # def __repr__(self):
-    #     return f"Mammal(name='{self.name}', age={self.age})"
+    def __repr__(self):
+        return f"Mammal(name='{self.name}', age={self.age})"
 
-    # def __str__(self):
-    #     return f"{self.name}, a {self.age}-year-old mammal."
+    def __str__(self):
+        return f"{self.name}, a {self.age}-year-old mammal."
 
     def __add__(self, other):
         if isinstance(other, Mammal):
@@ -39,9 +39,11 @@ class Mammal:
 
 
 class Dog(Mammal):
-    # def __init__(self, name, age, species_name):
-    #     super().__init__(name, age)
-    #     self.species_name = species_name
+    dog_class_name = 'Dog'
+
+    def __init__(self, name, age, type_name):
+        super().__init__(name, age)
+        self.type_name = type_name
 
     def bark(self):
         print("Barking")
@@ -52,14 +54,18 @@ class Cat(Mammal):
         print("Mewoing")
 
 
-# dog = Dog(name='大圣', age=5)
-# cat = Cat(name='南瓜', age=8)
-# print(Mammal.class_name, Mammal.warm_blooded)
+# dog = Dog(name='max', age=5, type_name='dog')
+# cat = Cat(name='pumpkin', age=8)
+# print(dog.type_name)
+# print(dog.dog_class_name)
+# print(dog.class_name)
+
+# print(Mammal.class_name, Mammal.warm_blood)
 # print(help(Mammal))
 # print(help(Dog))
 
 # 类的方法 (Class Methods)
-# Mammal.print_class_name()
+# print(Mammal.print_class_name())
 
 # 使用默认构建器
 # mammal1 = Mammal("Tom", 3)
@@ -73,14 +79,17 @@ class Cat(Mammal):
 # Mammal.introduction()
 
 # 特殊的方法 (Dunder Methods)
-# print(repr(dog))  # This will call leo.__repr__()
-# print(dog)  # This will call leo.__str__()
+# dog = Dog(name='max', age=5, type_name='dog')
+# print(repr(dog))  # This will call dog.__repr__()
+# print(dog)  # This will call dog.__str__()
 
 # 特殊方法 __add__ (多态性 Polymorphism)
 # print(int.__add__(1, 2))
+# print(1+2)
 # print(str.__add__('a', 'b'))
+# print('a'+'b')
 
-# mammal1 = Dog(name='大圣', age=5)
-# mammal2 = Cat(name='南瓜', age=8)
+# mammal1 = Dog(name='max', age=5, type_name='dog')
+# mammal2 = Cat(name='pumpkin', age=8)
 # new_mammal = mammal1 + mammal2
 # print(new_mammal.name, new_mammal.age)
